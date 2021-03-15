b53e3c1f3bca79a9d3bc563dd07b571049ec9405
Cody Hinz <codyhinz@gmail.com>
2021-03-15 15:39:37 -0400

    Updating for Heroku Deployment, hopefully this works.

--- model/database.php
+++ model/database.php
@@ -1,10 +1,11 @@
 <?php 
 
-    $dsn = 'mysql:host=localhost;dbname=zippysusedautos';
-    $username = 'root';
+    $dsn = 'mysql:host=lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=e68ad3ljivfy80mn';
+    $username = 'm5569wsb8xde1n17';
+    $password = 'z8m1avyx39p8l1kf';
 
     try {
-        $db = new PDO($dsn, $username);
+        $db = new PDO($dsn, $username, $password);
     } catch(PDOException $e) {
         echo $e;
     }
